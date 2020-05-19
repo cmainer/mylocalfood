737f4df1c2f0815214c9bd5f77f1d0b640454b84
Colton Malone <ctmalone15@gmail.com>
2020-05-18 19:58:53 -0600

    update database names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_020204) do
+ActiveRecord::Schema.define(version: 2020_05_12_042511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -204,6 +204,9 @@ ActiveRecord::Schema.define(version: 2020_03_26_020204) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.string "referral_code"
+    t.integer "referred_by_id"
+    t.datetime "referral_completed_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
