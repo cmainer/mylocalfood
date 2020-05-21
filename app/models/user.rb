7946a4d6a2cb4d162ed197291bf076e37e64337a
Colton Malone <ctmalone15@gmail.com>
2020-05-21 09:00:00 -0600

    fix logo on homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string
+#  last_name              :string
 #  invitation_accepted_at :datetime
 #  invitation_created_at  :datetime
 #  invitation_limit       :integer
@@ -20,7 +21,6 @@
 #  invitation_token       :string
 #  invitations_count      :integer          default(0)
 #  invited_by_type        :string
-#  last_name              :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
